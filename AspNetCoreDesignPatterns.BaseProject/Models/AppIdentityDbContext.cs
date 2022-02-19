b62099fb5b38f9d2844270a48a7fb8bf515998ec
Mert Güney <mert_guney_29@hotmail.com>
2022-02-19 18:03:42 +0300

    BaseIdentityProject and first migration created

---
+++ AspNetCoreDesignPatterns.BaseProject/Models/AppIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreDesignPatterns.BaseProject.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
